5560279c43ad0879058494d3d68f1ae7b5c9a1fd
Mukul Mantosh <mukulmantosh@yahoo.com>
2023-04-11 18:26:23 +0530

    Feat: Initial Commit

---
+++ polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
