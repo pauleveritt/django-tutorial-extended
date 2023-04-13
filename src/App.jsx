5d4e2833d4d3d38bc7163216dbddcbe675688071
pauleveritt <paul.everitt@jetbrains.com>
2023-04-13 09:06:10 -0400

    Remove unused assets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          Vite Logo
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          React Logo
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +26,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
