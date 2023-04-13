e2ab6dbf3aa0e3e4c6dd9769f16b31c4a4718520
pauleveritt <paul.everitt@jetbrains.com>
2023-04-13 13:22:01 -0400

    Get fetch in place with a test (though still need to mock fetch.)

--- src/Index.test.jsx
+++ src/Index.test.jsx
@@ -4,7 +4,15 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Index } from "./Index.jsx";
 
+global.fetch = vi.fn();
+
 test("load the polls on home page", async () => {
   render(<Index />);
-  expect(screen.getByRole("link", { name: "Vite Logo" })).to.exist;
+
+  // Ensure this renders by finding the h1
+  expect(screen.getByText("Questions")).to.exist;
+
+  // Find the questions
+  const first = screen.getAllByRole("link", { name: "Question" });
+  expect(first.href).to.equal(9);
 });
