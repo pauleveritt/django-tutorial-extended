3c1e9f69a1864f4707c7fb7e17d58ba5f625c3d3
Mukul Mantosh <mukulmantosh@yahoo.com>
2023-04-14 18:40:35 +0530

    Feat: - Make a root view

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
     path('v1/', include('mysite.apis.v1.urls')),
