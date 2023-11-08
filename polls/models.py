5edab2a05c9b901bd5724cd00d6b393638e979ad
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2023-11-08 16:30:33 +0100

    Updated Choice model to use db_default

--- polls/models.py
+++ polls/models.py
@@ -25,7 +25,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    votes = models.IntegerField(db_default=0)
 
     def __str__(self):
         return self.choice_text
